5b5e922226e2fe0f85b72ec9c644a277a96fead6
EllaNancyFay <ellaschofield0@gmail.com>
2015-01-10 12:44:32 +0000

    ping bell

---
+++ pingBell.js
@@ -0,0 +1,38 @@
+var five = require('johnny-five');
+var dgram = require('dgram');
+var board = new five.Board();
+var server = dgram.createSocket('udp4');
+
+board.on('ready', function() {
+  var piezo = new five.Piezo(8);
+  server.on('message', function(msg, rinfo) {
+    piezo.play({
+      song: [
+      ["C4", 1/4],
+      ["D4", 1/4],
+      ["F4", 1/4],
+      ["D4", 1/4],
+      ["A4", 1/4],
+      [null, 1/4],
+      ["A4", 1],
+      ["G4", 1],
+      [null, 1/2],
+      ["C4", 1/4],
+      ["D4", 1/4],
+      ["F4", 1/4],
+      ["D4", 1/4],
+      ["G4", 1/4],
+      [null, 1/4],
+      ["G4", 1],
+      ["F4", 1],
+      [null, 1/2]
+    ],
+    tempo: 100
+    });
+  })
+  server.bind(1337);
+});
+
+
+
+
